4a77303d3e3e6fdf1145f5e9374215c1078d6c15
Arunesh Mishra <aruneshm@gmail.com>
2016-11-14 21:33:43 -0800

    Create timeline

---
+++ app/src/main/java/codepath/travelbug/Utils.java
@@ -0,0 +1,97 @@
+package codepath.travelbug;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * Created by arunesh on 11/14/16.
+ */
+
+public class Utils {
+    public static final String PHOTO_FILE_PREFIX = "TravelBug";
+    public static final String APP_TAG = "TravelBug";
+    public static final String TAG = "TravelBug";
+    public static final String PIC_URI_KEY = "picUriKey";
+    public static final int MAX_WIDTH = 360 * 3; // xxxhdpi.
+
+    public static  String generateUniqueFileName(Random random) {
+        String filename = "";
+        long millis = System.currentTimeMillis();
+        String datetime = new Date().toGMTString();
+        datetime = datetime.replace(" ", "");
+        datetime = datetime.replace(":", "");
+        String rndchars = String.valueOf(random.nextLong());
+        filename = PHOTO_FILE_PREFIX + rndchars + "_" + datetime + "_" + millis;
+        Log.d(TAG, "Random filename = " + filename);
+        return filename;
+    }
+
+    public static boolean isExternalStorageAvailable() {
+        String state = Environment.getExternalStorageState();
+        return state.equals(Environment.MEDIA_MOUNTED);
+    }
+
+    // Returns the Uri for a photo stored on disk given the fileName
+    public static Uri getPhotoFileUri(Context context, String fileName) {
+        // Only continue if the SD Card is mounted
+        if (isExternalStorageAvailable()) {
+            // Get safe storage directory for photos
+            // Use `getExternalFilesDir` on Context to access package-specific directories.
+            // This way, we don't need to request external read/write runtime permissions.
+            File mediaStorageDir = new File(
+                    context.getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+            // Create the storage directory if it does not exist
+            if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+                Log.d(APP_TAG, "failed to create directory");
+            }
+
+            // Return the file target for the photo based on filename
+            return Uri.fromFile(new File(mediaStorageDir.getPath() + File.separator + fileName));
+        }
+        return null;
+    }
+
+    public static Bitmap scaleToFitWidth(Bitmap b, int width)
+    {
+        float factor = width / (float) b.getWidth();
+        return Bitmap.createScaledBitmap(b, width, (int) (b.getHeight() * factor), true);
+    }
+
+
+    // scale and keep aspect ratio
+    public static Bitmap scaleToFitHeight(Bitmap b, int height)
+    {
+        float factor = height / (float) b.getHeight();
+        return Bitmap.createScaledBitmap(b, (int) (b.getWidth() * factor), height, true);
+    }
+
+
+    // scale and keep aspect ratio
+    public static Bitmap scaleToFill(Bitmap b, int width, int height)
+    {
+        float factorH = height / (float) b.getWidth();
+        float factorW = width / (float) b.getWidth();
+        float factorToUse = (factorH > factorW) ? factorW : factorH;
+        return Bitmap.createScaledBitmap(b, (int) (b.getWidth() * factorToUse),
+                (int) (b.getHeight() * factorToUse), true);
+    }
+
+
+    // scale and don't keep aspect ratio
+    public static Bitmap strechToFill(Bitmap b, int width, int height)
+    {
+        float factorH = height / (float) b.getHeight();
+        float factorW = width / (float) b.getWidth();
+        return Bitmap.createScaledBitmap(b, (int) (b.getWidth() * factorW),
+                (int) (b.getHeight() * factorH), true);
+    }
+
+}
---
+++ app/src/main/java/codepath/travelbug/activities/CreateTimelineActivity.java
@@ -0,0 +1,64 @@
+package codepath.travelbug.activities;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOError;
+import java.io.IOException;
+
+import codepath.travelbug.R;
+import codepath.travelbug.Utils;
+
+import static codepath.travelbug.Utils.MAX_WIDTH;
+
+public class CreateTimelineActivity extends AppCompatActivity {
+    Uri pictureUri;
+    ImageView picView;
+    Button saveButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_timeline);
+        pictureUri = getIntent().getExtras().getParcelable(Utils.PIC_URI_KEY);
+        picView = (ImageView)findViewById(R.id.ivCameraImage);
+        saveButton = (Button) findViewById(R.id.btnSaveTimeline);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(CreateTimelineActivity.this, "Timeline created.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
+
+    // Takes the given image from the camera, resizes it and writes it to another file.
+    private void readImageIntoView(Uri pictureUri, ImageView imageView) throws IOException {
+        Bitmap takenImage = BitmapFactory.decodeFile(pictureUri.getPath());
+        Bitmap scaledImage = Utils.scaleToFitWidth(takenImage, MAX_WIDTH);
+        // Configure byte output stream
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        // Compress the image further
+        scaledImage.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+        // Create a new file for the resized bitmap (`getPhotoFileUri` defined above)
+        String resizedFilePath = pictureUri.getPath() + "_resized"));
+        File resizedFile = new File(resizedFilePath));
+        resizedFile.createNewFile();
+        FileOutputStream fos = new FileOutputStream(resizedFile);
+        // Write the bytes of the bitmap to file
+        fos.write(bytes.toByteArray());
+        fos.close();
+        imageView.setImageBitmap(scaledImage);
+    }
+}
