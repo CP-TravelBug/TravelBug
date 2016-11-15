64ea407887c0505c9a380292b01b694691b1a17b
Pragyan Debadarshini <aruneshm@gmail.com>
2016-11-14 21:24:34 -0800

    Added some Java files and TimelineActivity files.

---
+++ app/src/main/java/codepath/travelbug/models/Event.java
@@ -0,0 +1,67 @@
+package codepath.travelbug.models;
+
+import android.location.Geocoder;
+
+import java.util.List;
+
+/**
+ * Represents the Event class
+ * Is a unit of a user's creation and can be a video or a picture.
+ * Please refer to database schema or wiki for more information
+ */
+
+
+public class Event {
+
+
+    private long eventId;
+    private String path; // Path to content or video
+    private String content;
+    private List<Timeline> timelineList;
+    Geocoder location;
+
+    public long getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(long eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public List<Timeline> getTimelineList() {
+        return timelineList;
+    }
+
+    public void setTimelineList(List<Timeline> timelineList) {
+        this.timelineList = timelineList;
+    }
+
+    public Geocoder getLocation() {
+        return location;
+    }
+
+    public void setLocation(Geocoder location) {
+        this.location = location;
+    }
+
+
+
+
+
+}
---
+++ app/src/main/java/codepath/travelbug/models/Timeline.java
@@ -0,0 +1,12 @@
+package codepath.travelbug.models;
+
+/**
+ * Represents a user's timeline
+ * User can share a timeline with other users
+ * A timeline is viewed on the Timeline screen
+ */
+
+public class Timeline {
+
+
+}
