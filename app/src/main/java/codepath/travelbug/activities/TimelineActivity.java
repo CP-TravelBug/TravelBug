24ad2c605436c9bf149575d4a2b99b9c778da1c2
Arunesh Mishra <arunesh@arunesh-macbookpro2.roam.corp.google.com>
2016-11-14 21:33:43 -0800

    Create timeline

--- app/src/main/java/codepath/travelbug/activities/TimelineActivity.java
+++ app/src/main/java/codepath/travelbug/activities/TimelineActivity.java
@@ -1,7 +1,12 @@
 package codepath.travelbug.activities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Parcel;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,16 +26,32 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.parceler.Parcels;
 
+import java.util.Date;
+import java.util.Random;
+
 import codepath.travelbug.R;
+import codepath.travelbug.Utils;
 import codepath.travelbug.models.User;
 
+import static codepath.travelbug.Utils.PHOTO_FILE_PREFIX;
+import static codepath.travelbug.Utils.PIC_URI_KEY;
+import static codepath.travelbug.Utils.TAG;
+
 public class TimelineActivity extends AppCompatActivity {
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public final static int CREATE_TIMELINE_WITH_PIC_REQUEST_CODE = 1001;
+
+    private Random random;
+    private Uri lastCameraRequestUri;
+
     RoundedImageView ivProfileImage;
     TextView tvName;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        random = new Random();
         setContentView(R.layout.activity_timeline);
         ivProfileImage = (RoundedImageView)findViewById(R.id.ivProfileImage);
         tvName = (TextView)findViewById(R.id.tvName);
@@ -86,5 +107,48 @@ public class TimelineActivity extends AppCompatActivity {
         ivDisplayIcon = (ImageView) tileView.findViewById(R.id.gridImage);
         ivDisplayIcon.setImageResource(R.drawable.addfriends);
 
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab_createTimeline);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchCreateTimelineWithCamera();
+            }
+        });
+
+    }
+
+    // Launches the camera first to take a picture which is then passed to the
+    //  for creating a new timeline.
+    private void launchCreateTimelineWithCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        lastCameraRequestUri = Utils.getPhotoFileUri(this, Utils.generateUniqueFileName(random));
+        Log.d(TAG, "Image file URI:" + lastCameraRequestUri);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, lastCameraRequestUri); // set the image file name
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Uri takenPhotoUri = lastCameraRequestUri;
+                Intent i = new Intent(this, CreateTimelineActivity.class);
+                i.putExtra(PIC_URI_KEY, takenPhotoUri);
+                startActivityForResult(i, CREATE_TIMELINE_WITH_PIC_REQUEST_CODE);
+                // by this point we have the camera photo on disk
+                // Bitmap takenImage = BitmapFactory.decodeFile(takenPhotoUri.getPath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                // ImageView ivPreview = (ImageView) findViewById(R.id.ivPreview);
+                // ivPreview.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
