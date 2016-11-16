30cb1663f259ca219b194230810778caa82afcb3
Pragyan Debadarshini <aruneshm@gmail.com>
2016-11-15 17:49:08 -0800

    Added the mp4 file

--- app/src/main/java/codepath/travelbug/activities/ScrollingTimelineActivity.java
+++ app/src/main/java/codepath/travelbug/activities/ScrollingTimelineActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -46,18 +47,28 @@ public class ScrollingTimelineActivity extends AppCompatActivity {
         lvTimeline.setAdapter(adapter);
 
         displayTimeline();
+
+        if (adapter.isEmpty()) {
+            Toast.makeText(this, "No timelines created yet", Toast.LENGTH_LONG);
+        }
     }
 
     private void displayTimeline() {
         // Get Event objects and displays as timelines
         // Filler code until we read values from DB or server
         List<Timeline> timelines = Backend.get().getTimelines();
-        Timeline tm = timelines.get(0);
-        Event e = tm.getEventList().get(0);
-
-        eventLinkedList.add(e);
-
+        eventLinkedList.clear();
+        for (int i = 0; i < timelines.size(); i++) {
+            Timeline tm = timelines.get(i);
+            // ToDo add to specific timelines feature yet to be implemented
+            // For now each event is added to a new timeline
+            Event e = tm.getEventList().get(0);
+            eventLinkedList.add(e);
+        }
+        //adapter.clear();
         adapter.addAll(eventLinkedList);
+
+
 
     }
 }
