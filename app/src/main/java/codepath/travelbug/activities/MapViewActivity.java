b674e9afa38806f2b7bcad050a407d212291cf89
Pragyan <pragyan.bits@gmail.com>
2016-11-29 17:48:43 -0800

    Added the screen reecodring and .gif file

---
+++ app/src/main/java/codepath/travelbug/activities/MapViewActivity.java
@@ -0,0 +1,17 @@
+package codepath.travelbug.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import codepath.travelbug.R;
+import codepath.travelbug.backend.Backend;
+
+public class MapViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_view);
+
+    }
+}
