0d2599a296108ee322b36f9e4bd3befbc4ada068
Arunesh <aruneshm@gmail.com>
2016-12-08 12:42:39 -0800

    Add mapview to event view activity

--- app/src/main/java/codepath/travelbug/activities/EventViewActivity.java
+++ app/src/main/java/codepath/travelbug/activities/EventViewActivity.java
@@ -1,11 +1,22 @@
 package codepath.travelbug.activities;
 
 import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
@@ -13,15 +24,20 @@ import org.parceler.Parcels;
 import java.util.Date;
 
 import codepath.travelbug.R;
+import codepath.travelbug.adapter.CustomWindowAdapter;
 import codepath.travelbug.models.Event;
 import codepath.travelbug.models.User;
 
-public class EventViewActivity extends AppCompatActivity {
+import static codepath.travelbug.R.drawable.location;
+import static codepath.travelbug.TravelBugApplication.TAG;
 
+public class EventViewActivity extends AppCompatActivity implements OnMapReadyCallback {
     TextView tvEventText;
     ImageView ivEventImage;
     boolean hasGeoPoint = false;
     double lat, lng;
+    private GoogleMap mMap;
+    private Marker mMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +57,36 @@ public class EventViewActivity extends AppCompatActivity {
 
         tvEventText.setText(eventTitle);
         Picasso.with(this).load(eventUri).centerCrop().fit().into(ivEventImage);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        if (hasGeoPoint) {
+            mapFragment.getMapAsync(this);
+        } else {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.hide(mapFragment);
+            fragmentTransaction.commit();
+        }
+    }
+
+    private void addMarkerToMap() {
+        if (!hasGeoPoint) return;
+        LatLng currLatLng = new LatLng(lat, lng);
+        BitmapDescriptor defaultMarker = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED);
+        mMarker = mMap.addMarker(new MarkerOptions().position(currLatLng)
+                .icon(defaultMarker));
+        mMarker.setDraggable(true);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currLatLng, 12));
+        mMap.setInfoWindowAdapter(new CustomWindowAdapter(getLayoutInflater()));
+        Log.d(TAG, Double.toString(lat));
+        Log.d(TAG, Double.toString(lng));
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        if (hasGeoPoint) {
+            mMap = googleMap;
+            addMarkerToMap();
+        }
     }
 }
